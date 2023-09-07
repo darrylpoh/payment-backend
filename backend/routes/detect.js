6e98fd3320a1cdcb5ca37afe41ec807804244bc1
Yee Sen :) <ystan98@hotmail.com>
2023-09-07 10:06:00 +0800

    updated with /dashboard

--- backend/routes/detect.js
+++ backend/routes/detect.js
@@ -48,6 +48,8 @@ router.get('/', verifyToken,async (req, res) => {
 
 
 console.log(jsonData)
+
+
 const usdAmtValues = jsonData.map(item => parseFloat(item.usd_amt));
 const averageValues = jsonData.map(item => parseFloat(item.average));
 const numPoints = jsonData.length;
@@ -103,6 +105,149 @@ if (score>=0.8){
 //   "error": false,
 //   "message": "History fetched successfully"
 // };
+
+});
+
+
+
+
+router.get('/dashboard',async (req, res) => {
+  try {
+
+    // only USD_amt will be considered to ensure consistency when detecting anomaly
+
+    const history = await Transaction.findAll({
+      attributes: ['usd_amt', 'transaction_id', 'is_top_up', 'sender_id'],
+      where: {
+        [Op.and]: [,
+
+          {
+            is_top_up: false
+          }
+          ,],
+      },
+      
+      order: [['transaction_date', 'ASC']],
+    });
+
+    // Extract the plain data from Sequelize instances
+    const jsonData = history.map(transaction => transaction.get({ plain: true }));
+
+
+    // Create an object to store user data
+    const userTransactions = {};
+
+    // Loop through all data and group by sender
+    jsonData.forEach((transaction) => {
+      var { sender_id, usd_amt, transaction_id, is_top_up } = transaction;
+      
+      if (!userTransactions[sender_id]) {
+        userTransactions[sender_id] = [];
+      }
+
+      userTransactions[sender_id].push({
+        usd_amt,
+        transaction_id,
+        is_top_up,
+      });
+    });
+
+
+    // Here, i make the grouped data into a list of JSON Object
+const userList = Object.entries(userTransactions).map(([sender_id, transactions]) => ({
+    sender_id,
+    transactions,
+  }));
+
+
+  records = []
+ 
+  // Loop through each user in userList
+userList.forEach((user) => {
+  var result = {};
+  var { sender_id, transactions, transaction_id } = user;
+
+  for (let i = 0; i < transactions.length; i++) {
+    let sum = 0;
+    let count = 0;
+
+    for (let j = i; j >= 0 && j > i - 5; j--) {
+      sum += parseFloat(transactions[j].usd_amt);
+      count++;
+    }
+    
+    var average = sum / count;
+    transactions[i].average = average.toFixed(2);
+    transactions[i].userId = sender_id
+
+  }
+  const usd_amt = transactions.map((transaction) => parseFloat(transaction.usd_amt));
+  const avg_amt = transactions.map((transaction) => parseFloat(transaction.average));
+
+
+  const data = [];
+
+  for (let i = 0; i < usd_amt.length; i++) {
+    data.push([usd_amt[i], avg_amt[i]]);
+  }
+
+  const forest = new IsolationForest();
+  forest.fit(data);
+
+  for (let i = 0; i < usd_amt.length; i++) {
+    var anomaly = false;
+
+    // if >=0.8 is an anomaly.
+
+    if (forest.scores()[i]>=0.8) {
+      anomaly= true;
+    }
+
+    result[i] = {
+      user: sender_id,
+      amt: usd_amt[i],
+      avg_amt: avg_amt[i],
+      scores: forest.scores()[i],
+      anomaly: anomaly
+    };
+  }
+  records.push(result)
+
+});
+
+
+// Make and reorganize the output nicer for frontend
+const usersData = [];
+
+records.forEach((userTransactions) => {
+  const userId = userTransactions['0'].user;
+  const transactions = [];
+
+  for (const key in userTransactions) {
+    if (Object.hasOwnProperty.call(userTransactions, key)) {
+      const transaction = userTransactions[key];
+      const { user, ...transactionData } = transaction;
+      transactions.push(transactionData);
+    }
+  }
+  const userObject = {
+    user_id: userId,
+    transactions: transactions,
+  };
+  usersData.push(userObject);
+});
+
+res.json({'results':usersData});
+
+
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      error: true,
+      message: "Error",
+    });
+  }
 
 });
 
